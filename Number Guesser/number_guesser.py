3d7de609236adada8c8fc6009196bb8f72b7bcb7
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2020-05-15 11:21:34 -0400

    Update number_guesser.py

--- Number Guesser/number_guesser.py
+++ Number Guesser/number_guesser.py
@@ -4,29 +4,36 @@ import sys
 number = random.randint(1, 100)
 guess_count = 1
 print("""Let's play a guessing game. 
-Guess a number between 1 and 100 (inclusively).
-You have 10 chances to guess the number and 
-you will be instructed if your guess is too high or too low. """)
+Guess a number between 1 and 100.
+You have 10 chances to guess the number.
+You will be instructed if your guess is too high or too low. 
+Only valid guesses will be counted""")
 while guess_count <= 10:
     guess = input()
     if guess.isdigit():
         guess = int(guess)
-        if guess == number:
-            print("That is correct!\n")
-            break
-        elif guess < number:
-            guess_count += 1
-            print('Your guess was too LOW. Please try again!')
-        elif guess > number:
-            guess_count += 1
-            print("Your guess was too HIGH. Please try again")
+        if 1 <= guess <= 100:
+            if guess == number:
+                print("That is correct!\n")
+                break
+            elif guess < number:
+                guess_count += 1
+                print('Your guess was too LOW. Please try again!')
+            elif guess > number:
+                guess_count += 1
+                print("Your guess was too HIGH. Please try again")
+        else:
+            print("Invalid input. Please try again!")
     else:
         print("Invalid input. Please try again!")
+
+print()
 if guess_count == 1:
-    print("Congratulations it only took {} guess\n".format(guess_count), file=sys.stderr)
+    print("Congratulations it only took {} guess".format(guess_count), file=sys.stderr)
 elif guess_count <= 10:
-    print("Congratulations it only took {} guesses\n".format(guess_count), file=sys.stderr)
+    print("Congratulations it only took {} guesses".format(guess_count), file=sys.stderr)
 else:
     print("The correct number was {}".format(number), file=sys.stderr)
 
+print()
 input('Press Enter to exit')
